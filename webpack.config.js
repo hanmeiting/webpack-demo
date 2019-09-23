6ec025c6a3a7bf1b5df77b677c77bbb7f453d477
韩婷 <hanting0220@126.com>
2019-09-23 21:25:26 +0800

    多出口多入口

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
 const path = require("path");//node系统模块，__dirname也是
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
 	//入口文件
 	entry:{
-		entry_name:"./src/index.js" //可以随便定义
+		index:"./src/index.js",
+		index2:"./src/index2.js"
+		//可以随便定义
 	},
 	//出口文件
 	output:{
 		path:path.resolve(__dirname,"dist"), //要绝对路径
-		filename:"bundle.js" //定死的名字
-	}
+		filename:"[name].bundle.js" //定死的名字
+	},
+	plugins:[
+		new HtmlWebpackPlugin({
+			title:"I Love_China",
+			template:"./src/index.html",
+			hash:"true",
+			minify:{
+				collapseWhitespace:true,
+				removeAttributeQuotes:true
+			}
+
+		})
+	]
 }
